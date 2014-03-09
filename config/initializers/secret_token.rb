c99b6ebf2afa8d4f2c4d14b6e51dff5bd73477cc
Hongchao Liu <lbs.lhc@gmail.com>
2014-03-09 22:39:19 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonadexIo::Application.config.secret_key_base = '26b7c798a045a4b5f4d794c98deb506df751b168982f4f9005b6b4b79ffe61d037b255b85ebe8be9aa2ac5baddda5aee2588d61b9d5e4845ea92a88327b07597'
